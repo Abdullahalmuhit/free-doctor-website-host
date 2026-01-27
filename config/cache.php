8a60d6fe118d1fcaf836fa6490a34a85bbb2830e
Muhid <abdullahalmuhit1296@gmail.com>
2026-01-27 16:41:29 +0600

    Added cache

--- config/cache.php
+++ config/cache.php
@@ -49,7 +49,7 @@ return [
 
         'file' => [
             'driver' => 'file',
-            'path' => storage_path('framework/cache/data'),
+            'path' => env('CACHE_PATH', storage_path('framework/cache/data')),
             'lock_path' => storage_path('framework/cache/data'),
         ],
 
